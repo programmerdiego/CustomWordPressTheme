e75509965190aae977bb97a6c489f51fb1d15b18
Diego Sanchez <sanchezdiego@email.com>
2018-08-07 13:06:55 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,35 @@
+<?php
 
+add_action('customize_register', function($wpcm) {
+    $wpcm->add_setting('bgcolor', array(
+        'default' => 'white'
+      ));
+  
+    $wpcm->add_setting('textcolor', array(
+      'default' => 'black'
+    
+   )); 
+  $wpcm->add_control('bgcolor-contro', array(
+     'lable' => 'Background color', 
+     'section' => 'colors', 
+     'settings' => 'bgcolor'
+  )); 
+                      
+  $wpcm->add_control('textcolor-control', array(
+    'label' => 'Text Color',
+    'section' => 'colors',
+    'settings' => 'textcolor'
+  )); 
+  
+  
+});
+add_action('wp_head', function() { ?>
+ <style type='text/css'>
+    body {
+      background-color: <?php echo get_theme_mod('bgcolor'); ?>;
+        color: <?php echo get_theme_mod('textcolor'); ?>;
+   }
+</style>
+      
+  
+<?php });
