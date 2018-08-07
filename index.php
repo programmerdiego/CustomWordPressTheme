0fa7fa66b88f6f50634282dd1ed89f6886d2651d
Diego Sanchez <sanchezdiego@email.com>
2018-08-07 12:07:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php bloginfo('title'); ?></title>
+  </head>
+  <body>
+    <h1><?php bloginfo('title'); ?></h1>
+    <h3><?php bloginfo('description'); ?></h3>
+    
+    <h5>Post Date</h5>
+    <h2>Post Title</h2>
+    <p>Post's content</p>
+    <hr/>
+  </body>
+</html>
