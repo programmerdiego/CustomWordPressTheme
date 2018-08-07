6be8b9b18742e718aea70816a0b48d7e8f020c04
Diego Sanchez <sanchezdiego@email.com>
2018-08-07 12:35:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
     <h1><?php bloginfo('title'); ?></h1>
     <h3><?php bloginfo('description'); ?></h3>
     
-    <h5>Post Date</h5>
-    <h2>Post Title</h2>
-    <p>Post's content</p>
-    <hr/>
+    <?php while(have_posts()) {
+      the_post();
+    ?>
+      <h5><?php the_date(); ?></h5>
+      <h2>
+        <a href="index.php?p=<?php the_ID(); ?>">
+            <?php the_title(); ?>
+        </a>
+       </h2>
+      <p><?php the_content(); ?></p>
+      <hr/>
+    <?php } ?>
   </body>
 </html>
